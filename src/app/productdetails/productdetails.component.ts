4bb0c114ae9490f2e1cbc4af26a81b336ac3b9de
mizuki-akimoto216 <119698616+mizuki-akimoto216@users.noreply.github.com>
2023-08-03 16:40:50 -0700

    added activated route

--- src/app/productdetails/productdetails.component.ts
+++ src/app/productdetails/productdetails.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AnyARecord, AnyMxRecord, AnyNaptrRecord } from 'dns';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ns-productdetails',
   templateUrl: './productdetails.component.html',
 })
-export class ProductdetailsComponent {
+export class ProductdetailsComponent implements OnInit {
+  //we check for the product id in the URL
+  //take the id and pass the id to the Strapi API
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    let productID = this.route.snapshot.paramMap.get('id');
+    console.log('product id', productID);
+  }
 }
