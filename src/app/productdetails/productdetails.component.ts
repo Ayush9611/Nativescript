7803b27be47f17ea1961006035f313836fcd9213
mizuki-akimoto216 <119698616+mizuki-akimoto216@users.noreply.github.com>
2023-08-03 17:06:32 -0700

    add routerlink

--- src/app/productdetails/productdetails.component.ts
+++ src/app/productdetails/productdetails.component.ts
@@ -3,6 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { AnyARecord, AnyMxRecord, AnyNaptrRecord } from 'dns';
 import { ActivatedRoute } from '@angular/router';
 
+interface Product {
+  id: number;
+  attributes: {
+    ProductName: string;
+    Price: string;
+    ProductDescription: string;
+  };
+}
 @Component({
   selector: 'ns-productdetails',
   templateUrl: './productdetails.component.html',
@@ -10,10 +18,21 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductdetailsComponent implements OnInit {
   //we check for the product id in the URL
   //take the id and pass the id to the Strapi API
-  constructor(private route: ActivatedRoute) {}
+  product: Product;
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit() {
     let productID = this.route.snapshot.paramMap.get('id');
-    console.log('product id', productID);
+
+    this.http
+      .get<Product>(
+        'https://starfish-app-ux8rd.ondigitalocean.app/api/products' +
+          productID +
+          '?populate=*'
+      )
+      .subscribe((res) => {
+        console.log('product', res);
+        this.product = res;
+      });
   }
 }
