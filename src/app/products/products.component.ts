3380ec91da76ddb41f366ffa625edbb67292d189
mizuki-akimoto216 <119698616+mizuki-akimoto216@users.noreply.github.com>
2023-08-21 17:37:56 -0700

    add top

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,19 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AnyARecord, AnyMxRecord, AnyNaptrRecord } from 'dns';
 
+interface Top {
+  data: {
+    id: number;
+    attributes: {
+      backgroundImg: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      topText: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      topImg: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+    };
+  };
+}
+
 @Component({
   selector: 'ns-products',
   templateUrl: './products.component.html',
 })
 export class ProductsComponent implements OnInit {
   productsapi: any[] = [];
-  product: any[] = [];
+  productList: any[] = [];
   top: any[] = [];
+  backgroundImg: string = '';
+  topText: string = '';
+  topImg: string = '';
 
   constructor(private http: HttpClient) {}
 
   getProductDetails(id: Number) {
-    console.log('ID', id);
+    // console.log('ID', id);
   }
 
   ngOnInit() {
@@ -23,22 +55,26 @@ export class ProductsComponent implements OnInit {
         'https://starfish-app-ux8rd.ondigitalocean.app/api/products?populate=*'
       )
       .subscribe((res) => {
-        console.log(res);
+        // console.log(res);
         this.productsapi = res.data;
       });
 
     this.http
       .get<any>('https://cms.mizukiakimoto.me/api/products?populate=*')
       .subscribe((res) => {
-        console.log(res);
-        this.product = res.data;
+        // console.log(res);
+        this.productList = res.data;
       });
 
     this.http
-      .get<any>('https://cms.mizukiakimoto.me/api/mobile-top?populate=*')
+      .get<Top>('https://cms.mizukiakimoto.me/api/mobile-top?populate=*')
       .subscribe((res) => {
-        console.log(res);
-        this.top = res.data;
+        // console.log(res);
+        this.backgroundImg =
+          res.data.attributes.backgroundImg.data.attributes.url;
+        this.topText = res.data.attributes.topText.data.attributes.url;
+        this.topImg = res.data.attributes.topImg.data.attributes.url;
+        console.log(this.backgroundImg);
       });
   }
 }
