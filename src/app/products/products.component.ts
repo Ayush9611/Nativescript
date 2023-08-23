4fdcb2b5417443803e22d3b87a62fc4ed25c6e0c
mizuki-akimoto216 <119698616+mizuki-akimoto216@users.noreply.github.com>
2023-08-22 17:16:50 -0700

    2 files modified

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -31,6 +31,98 @@ interface Top {
   };
 }
 
+interface MobileImage {
+  data: {
+    id: number;
+    attributes: {
+      productTitle: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      aboutTitle: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      aboutImage: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      orderTitle: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      orderNum1: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      orderNum2: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      orderNum3: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      newsTitle: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      logo: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      twitter: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      instagram: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+      facebook: {
+        data: {
+          attributes: {
+            url: string;
+          };
+        };
+      };
+    };
+  };
+}
+
 @Component({
   selector: 'ns-products',
   templateUrl: './products.component.html',
@@ -42,11 +134,23 @@ export class ProductsComponent implements OnInit {
   backgroundImg: string = '';
   topText: string = '';
   topImg: string = '';
+  productTitle: string = '';
+  aboutTitle: string = '';
+  aboutImage: string = '';
+  orderTitle: string = '';
+  orderNum1: string = '';
+  orderNum2: string = '';
+  orderNum3: string = '';
+  newsTitle: string = '';
+  logo: string = '';
+  twitter: string = '';
+  instagram: string = '';
+  facebook: string = '';
 
   constructor(private http: HttpClient) {}
 
   getProductDetails(id: Number) {
-    // console.log('ID', id);
+    console.log('ID', id);
   }
 
   ngOnInit() {
@@ -75,6 +179,26 @@ export class ProductsComponent implements OnInit {
         this.topText = res.data.attributes.topText.data.attributes.url;
         this.topImg = res.data.attributes.topImg.data.attributes.url;
         console.log(this.backgroundImg);
+      });
+
+    this.http
+      .get<MobileImage>(
+        'https://cms.mizukiakimoto.me/api/mobile-image?populate=*'
+      )
+      .subscribe((res) => {
+        this.productTitle =
+          res.data.attributes.productTitle.data.attributes.url;
+        this.aboutTitle = res.data.attributes.aboutTitle.data.attributes.url;
+        this.aboutImage = res.data.attributes.aboutImage.data.attributes.url;
+        this.orderTitle = res.data.attributes.orderTitle.data.attributes.url;
+        this.orderNum1 = res.data.attributes.orderNum1.data.attributes.url;
+        this.orderNum2 = res.data.attributes.orderNum2.data.attributes.url;
+        this.orderNum3 = res.data.attributes.orderNum3.data.attributes.url;
+        this.newsTitle = res.data.attributes.newsTitle.data.attributes.url;
+        this.logo = res.data.attributes.logo.data.attributes.url;
+        this.twitter = res.data.attributes.twitter.data.attributes.url;
+        this.instagram = res.data.attributes.instagram.data.attributes.url;
+        this.facebook = res.data.attributes.facebook.data.attributes.url;
       });
   }
 }
